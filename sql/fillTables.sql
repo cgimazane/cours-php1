94898440844fc6a5dcf6bc4f796197b6136a6e2e
cgimazane <clement.gimazane@ign.fr>
2017-08-01 12:55:47 +0200

    edit sql

--- sql/fillTables.sql
+++ sql/fillTables.sql
@@ -1,11 +1,11 @@
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Tenrien','Jean','m','1992-12-12',false);
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Houarien','Yves','m','1992-12-12',true);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Tenrien','Jean','m','1791-12-26',false);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Houarien','Yves','m','2002-02-09',true);
 INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Tartine','Kimberley','f','1992-12-12',false);
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Hervitemonslip','Jean-Philippe','m','1992-12-12',false);
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Bolizan','Anna','f','1992-12-12',false);
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Javel','Aude','f','1992-12-12',true);
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Honnette','Marie','f','1992-12-12',true);
-INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Menvussa','Gérard','m','1992-12-12',false);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Hervitemonslip','Jean-Philippe','m','1989-05-03',false);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Bolizan','Anna','f','2000-02-29',false);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Javel','Aude','f','1965-07-04',true);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Honnette','Marie','f','1960-04-02',true);
+INSERT INTO personne (nom,prenom,sexe,naissance,lunettes) VALUES ('Menvussa','Gérard','m','1956-07-24',false);
 
 INSERT INTO bureau (nom,batiment,etage) VALUES ('L305','L',3);
 INSERT INTO bureau (nom,batiment,etage) VALUES ('K12','K',0);
